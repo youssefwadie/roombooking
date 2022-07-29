02dcba05fca96bf05b67d8897207370f9d21061f
Youssef Wadie <youssefwadie14@gmail.com>
2022-07-29 17:00:25 +0200

    initial components and menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { RoomsComponent } from './admin/rooms/rooms.component';
+import { UsersComponent } from './admin/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CalendarComponent,
+    RoomsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
